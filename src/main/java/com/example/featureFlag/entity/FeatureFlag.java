35f554ab9cf9eb3f26615cfd1ef347057fcfde9f
A3ryn69 <a3ryn69@gmail.com>
2025-12-17 08:02:18 +0530

    feat: add FeatureFlag entity with composite uniqueness

---
+++ src/main/java/com/example/featureFlag/entity/FeatureFlag.java
@@ -0,0 +1,39 @@
+package com.example.featureFlag.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name="feature_flags", uniqueConstraints = {@UniqueConstraint(columnNames = {"key", "environment"})})
+@Data
+public class FeatureFlag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String key;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @Column(nullable = false)
+    private String environment;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    private String createdBy;
+
+}
